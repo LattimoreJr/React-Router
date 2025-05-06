9df10c1c3d038dfe35ef7e2b9f2e0469032939b3
William Lattimore Jr <lattimorejr@Junior-M4Pro.local>
2025-05-05 22:37:38 -0700

    completed assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-
+import { Routes, Route, Link } from "react-router-dom"
+import default Red from "./Components/Red.jsx"
+import default Blue from "./Components/Blue.jsx"
 
 function App() {
  
 
   return (
     <div id="container">
-    <h1>Hello React Router!</h1>
-    <div id="navbar">{/* navigation here */}</div>
-    <div id="main-section">{/* routes here */}</div>
+    <div id="navbar">
+      <Link to="/red">{<h1>Red</h1>}</Link>
+      <Link to="/blue">{<h1>Blue</h1>}</Link>
+    </div>
+    <div id="main-section">
+      <Routes>
+        <Route path="/blue" element={<h1>Blue</h1>}/>
+        <Route path="/red" element={<h1>Red</h1>}/>
+      </Routes>
+    </div>
   </div>
   )
 }
